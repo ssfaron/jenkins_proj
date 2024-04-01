ca6764f1fcc61478072b6154ffff32580c7d948f
ssfaron <slsmallw.asu@gmail.com>
2024-03-31 22:59:08 -0500

    first commit

---
+++ src/main/java/cicd/jenkins_proj/JenkinsProjApplication.java
@@ -0,0 +1,13 @@
+package cicd.jenkins_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cicd/jenkins_proj/JenkinsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.jenkins_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
